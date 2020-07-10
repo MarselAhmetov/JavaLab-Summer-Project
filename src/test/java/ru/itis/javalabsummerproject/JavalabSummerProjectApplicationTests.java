d40e0c58b9a3761c3a1c49ba9c6f51a38ec41a29
MarselAhmetov <marsel5027@gmail.com>
2020-07-10 23:52:08 +0300

    initial commit

---
+++ src/test/java/ru/itis/javalabsummerproject/JavalabSummerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.javalabsummerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavalabSummerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
