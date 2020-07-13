cbc31ab123b277323ab37e532910e4c485536935
Vladislav <Vladislav4869@gmail.com>
2020-07-13 12:01:49 +0300

    Merge with models

--- src/main/java/ru/itis/javalabsummerproject/model/User.java
+++ src/main/java/ru/itis/javalabsummerproject/model/User.java
@@ -1,25 +1,23 @@
 package ru.itis.javalabsummerproject.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
 import ru.itis.javalabsummerproject.model.enamuration.Role;
 
-import javax.persistence.*;
-import java.io.Serializable;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.List;
 
 @Entity
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String username;
     private String password;
     private String email;
     @ElementCollection(fetch = FetchType.EAGER)
