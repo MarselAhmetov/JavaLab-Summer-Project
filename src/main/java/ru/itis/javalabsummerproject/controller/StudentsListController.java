9de59d318d4ab54dc96510b203bd35cba30608cb
Sergey Mikheev <mikheevs11@gmail.com>
2020-07-23 13:22:13 +0300

    Updated css successfully for students.ftlh

--- src/main/java/ru/itis/javalabsummerproject/controller/StudentsListController.java
+++ src/main/java/ru/itis/javalabsummerproject/controller/StudentsListController.java
@@ -6,8 +6,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+import ru.itis.javalabsummerproject.model.Resume;
+import ru.itis.javalabsummerproject.model.Student;
+import ru.itis.javalabsummerproject.repositories.ResumeRepository;
 import ru.itis.javalabsummerproject.security.UserDetailsImpl;
 import ru.itis.javalabsummerproject.service.interfaces.StudentService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class StudentsListController {
@@ -15,10 +22,22 @@ public class StudentsListController {
     @Autowired
     private StudentService studentService;
 
+    @Autowired
+    private ResumeRepository resumeRepository;
+
     @GetMapping("/students")
     public ModelAndView getVacanciesPage(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         ModelAndView modelAndView = new ModelAndView("students");
-        modelAndView.addObject("students", studentService.findAll());
+
+        Map<Student, List<Resume>> studentListMap = new HashMap<>();
+
+        List<Student> studentList =  studentService.findAll();
+        for(Student student: studentList) {
+            List<Resume> resumeList = resumeRepository.getAllByUser(student.getUser());
+            studentListMap.put(student, resumeList);
+        }
+
+        modelAndView.addObject("studentsMap", studentListMap);
         modelAndView.addObject("principal", userDetails);
         return modelAndView;
     }
